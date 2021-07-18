d753d08841ee50793e202a0bbac1f668b28f9cec
Kam <kamtoto.au@gmail.com>
2021-07-18 23:19:12 +0800

    Initial Commit

---
+++ Benchmark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Benchmark
+//
+//  Created by Kam on 2021/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
